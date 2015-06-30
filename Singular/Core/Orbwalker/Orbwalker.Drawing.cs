425b1c5fdb128d19eaf001f88008336da4e0b973
JelloBeans <jello.beans.sharp@gmail.com>
2015-06-30 17:41:03 +0100

    Add updated dll files

--- Singular/Core/Orbwalker/Orbwalker.Drawing.cs
+++ Singular/Core/Orbwalker/Orbwalker.Drawing.cs
@@ -23,31 +23,31 @@
         private static void Orbwalker_Drawing_OnDraw(System.EventArgs args)
         {
             var minions = GameObjects.EnemyMinions.ToList();
-            foreach (var minion in minions.Where(m => m.IsHPBarRendered && m.IsValidTarget()))
-            {
-                var attackDamage = Me.GetAutoAttackDamage(minion, true);
+            //foreach (var minion in minions.Where(m => m.IsHPBarRendered && m.IsValidTarget()))
+            //{
+            //    var attackDamage = Me.GetAutoAttackDamage(minion, true);
 
-                var healthBar = minion.HPBarPosition;
-                var maxHealth = minion.MaxHealth;
-                var currentHealth = minion.Health;
+            //    var healthBar = minion.HPBarPosition;
+            //    var maxHealth = minion.MaxHealth;
+            //    var currentHealth = minion.Health;
 
-                if (currentHealth <= attackDamage)
-                {
-                    continue;
-                }
+            //    if (currentHealth <= attackDamage)
+            //    {
+            //        continue;
+            //    }
 
-                var barWidth = healthBar.Length();
+            //    var barWidth = healthBar.Length();
 
-                var split = (float)(barWidth / (maxHealth / attackDamage));
-                var offset = barWidth - split;
-                while (offset > 0)
-                {
-                    var startVector = new Vector2(healthBar.X + 45 + offset, healthBar.Y + 18);
-                    var endVector = new Vector2(healthBar.X + 45 + offset, healthBar.Y + 23);
-                    Drawing.DrawLine(startVector, endVector, 1f, Color.White);
-                    offset -= split;
-                }
-            }
+            //    var split = (float)(barWidth / (maxHealth / attackDamage));
+            //    var offset = barWidth - split;
+            //    while (offset > 0)
+            //    {
+            //        var startVector = new Vector2(healthBar.X + 45 + offset, healthBar.Y + 18);
+            //        var endVector = new Vector2(healthBar.X + 45 + offset, healthBar.Y + 23);
+            //        Drawing.DrawLine(startVector, endVector, 1f, Color.White);
+            //        offset -= split;
+            //    }
+            //}
         }
     }
 }
